f3da25919eeac1a12709d2481ba83fec8bf06d31
MShinskiy <88272807+MShinskiy@users.noreply.github.com>
2022-08-02 23:29:31 +0300

    update classes

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -6,14 +6,12 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.*;
 
 public class Bot extends TelegramLongPollingBot {
 
-    private final ConcurrentHashMap<Integer, Integer> userState = new ConcurrentHashMap<>();
     private int state = 0;
+
     @Override
     public String getBotUsername() {
         return "EatsFastBot";
@@ -26,47 +24,133 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText())
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            System.out.println("Message received: " + update.getMessage().getText());
             stateHandler(update);
+            System.out.println("Message answered, current state = " + state);
+        }
     }
 
-    private void stateHandler(Update update){
-        if(this.state == 1)
-            switch (update.getMessage().getText()){
+    private void stateHandler(Update update) {
+        if (Objects.equals(update.getMessage().getText(), "/start"))
+            startupMessage(update);
+        if (this.state == 1)
+            switch (update.getMessage().getText()) {
                 case "Add order":
                     this.state = 2;
                     break;
-                case "View order":
-                    this.state = 5;
+                case "View orders":
+                    this.state = 4;
                     break;
             }
-        if(update.getMessage().getText() == "/start")
-            this.state = 1;
-        switch (state){
-            case 1:
-                startupMessage(update);
-                break;
+        else if(this.state == 5)
+            switch (update.getMessage().getText()) {
+                case "Today's orders":
+                    viewTodayOrders(update);
+                    break;
+                case "My orders":
+                    viewMyOrders(update);
+                    break;
+                case "Unchecked orders":
+                    viewUncheckedOrders(update);
+                    break;
+            }
+
+        switch (this.state) {
             case 2:
-                getOrder();
+                getOrder(update);
                 break;
             case 3:
                 addOrder(update);
                 break;
             case 4:
-                notifyCouriers();
+                viewOrder(update);
                 break;
             case 5:
-                viewOrder(update);
+                showOrdersMessage(update);
                 break;
         }
 
     }
+
     //state 1
     private void startupMessage(Update update) {
+        sendMessageForState1(update, "Welcome!");
+    }
+
+    //state 2
+    private void getOrder(Update update) {
+        SendMessage message = new SendMessage();
+        message.setChatId(update.getMessage().getChatId().toString());
+        message.setText("Writing down your order...");
+        try {
+            execute(message);
+            this.state = 3;
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //state 3
+    private void addOrder(Update update) {
+        String order = update.getMessage().getText();
+        /*
+        TODO
+            connect to db and add order to orders table
+         */
+        SendMessage message = new SendMessage();
+        message.setChatId(update.getMessage().getChatId().toString());
+        message.setText("Order added.");
+        try {
+            execute(message);
+            notifyCouriers(update);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //state 4
+    private void viewOrder(Update update) {
         //build message
         SendMessage message = new SendMessage();
         message.setChatId(update.getMessage().getChatId().toString());
-        message.setText("Welcome!");
+        message.setText("What orders would you like to view?");
+
+        //build keyboard
+        List<KeyboardRow> list = Arrays.asList(
+                new KeyboardRow(new ArrayList<>(Collections.singletonList(new KeyboardButton("Today's orders")))),
+                new KeyboardRow(new ArrayList<>(Collections.singletonList(new KeyboardButton("My orders")))),
+                new KeyboardRow(new ArrayList<>(Collections.singletonList(new KeyboardButton("Unchecked orders"))))
+        );
+        ReplyKeyboardMarkup ordersMessageRKM = new ReplyKeyboardMarkup();
+        ordersMessageRKM.setKeyboard(list);
+        ordersMessageRKM.setInputFieldPlaceholder("Placeholder");
+        ordersMessageRKM.setOneTimeKeyboard(true);
+        ordersMessageRKM.setResizeKeyboard(true);
+        message.setReplyMarkup(ordersMessageRKM);
+
+        //execute message
+        try{
+            execute(message);
+            this.state = 5;
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    //state 5
+    private void showOrdersMessage(Update update) {
+        sendMessageForState1(update, "Your orders.");
+    }
+
+    //Support methods=======================================================
+    //send message for option state (state = 1)
+    private void sendMessageForState1(Update update, String messageText) {
+        //build message
+        SendMessage message = new SendMessage();
+        message.setText(messageText);
+        message.setChatId(update.getMessage().getChatId().toString());
 
         //build keyboard
         //buttons
@@ -84,55 +168,69 @@ public class Bot extends TelegramLongPollingBot {
         startMessageRKM.setOneTimeKeyboard(true);
         message.setReplyMarkup(startMessageRKM);
 
-        //execute
+        //execute message
         try {
+            execute(message);
+            this.state = 1;
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //view orders methods---------------------------------------------------
+    private void viewTodayOrders(Update update) {
+        /*
+        TODO
+            Show list of today's orders
+         */
+        SendMessage message = new SendMessage();
+        message.setChatId(update.getMessage().getChatId().toString());
+        message.setText("Today's orders.");
+        try{
             execute(message);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
 
-    //state 2
-    private void getOrder() {
-        SendMessage message = new SendMessage();
-        message.setText("Writing down your order...");
-        try{
-            execute(message);
-            this.state = 3;
-        } catch (TelegramApiException e){
-            e.printStackTrace();
-        }
-    }
-
-    //state 3
-    private void addOrder(Update update) {
-        String order = update.getMessage().getText();
-        /*TODO
-            connect to db and add order to orders table
+    private void viewMyOrders(Update update) {
+        /*
+        TODO
+            Show list of my orders
          */
         SendMessage message = new SendMessage();
-        message.setText("Order added.");
+        message.setChatId(update.getMessage().getChatId().toString());
+        message.setText("My orders.");
         try{
             execute(message);
-            this.state = 4;
-        } catch (TelegramApiException e){
+        } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
+    private void viewUncheckedOrders(Update update) {
+        /*
+        TODO
+            Show list of unchecked orders
+         */
+        SendMessage message = new SendMessage();
+        message.setChatId(update.getMessage().getChatId().toString());
+        message.setText("Unchecked orders.");
+        try{
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    //----------------------------------------------------------------------
 
-    //state 4
-    private void notifyCouriers() {
-        /*TODO
+    //Notifying couriers that new order has been placed
+    private void notifyCouriers(Update update) {
+        /*
+        TODO
             notify method
          */
-        this.state = 1;
+        sendMessageForState1(update, "Couriers notified!");
     }
-
-    //state 5
-    private void viewOrder(Update update){
-
-    }
-
 
     //echo
     private void echo(Update update) {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,20 +8,19 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-
-        try {
-            //instantiate bot
-            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            //register bot
-            botsApi.registerBot(new Bot());
-        } catch (TelegramApiException e){
-            e.printStackTrace();
-        }
-
         try(Connection conn = HerokuConnection.getConnection()){
 
             if(conn != null)
                 System.out.println("Connected");
+
+            try {
+                //instantiate bot
+                TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+                //register bot
+                botsApi.registerBot(new Bot());
+            } catch (TelegramApiException e){
+                e.printStackTrace();
+            }
 
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
